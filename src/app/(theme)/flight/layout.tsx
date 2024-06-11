e4432d76159692a7898dc5668a7149b390f9ba15
Arup Maity <arupmaity079@gmail.com>
2024-06-11 17:36:49 +0530

    fljj

--- src/app/(theme)/flight/layout.tsx
+++ src/app/(theme)/flight/layout.tsx
@@ -1,18 +1,13 @@
 import Footer from "@/components/theme/footer";
 import Header from "@/components/theme/header/Header";
-import { Suspense } from 'react'
-interface PropsType {
-   children: React.ReactNode;
-}
+
 export default function ThemeLayout({ children }: { children: React.ReactNode }) {
    return (
-      <Suspense fallback={<div>Loading...</div>}>
          <div className="w-full">
             <Header />
             {children}
             <Footer />
          </div>
-      </Suspense>
 
    )
 }
--- src/app/(theme)/flight/page.tsx
+++ src/app/(theme)/flight/page.tsx
@@ -2,6 +2,7 @@ import FlightHero from '@/components/theme/flight/FlightHero'
 import OfferMap from '@/components/theme/flight/OfferMap'
 import Link from 'next/link'
 import React from 'react'
+import { Suspense } from 'react'
 
 const flightOffers = [
    {
@@ -36,80 +37,82 @@ const flightOffers = [
 
 const Flight = () => {
    return (
-      <div className='w-full mb-10'>
-         <FlightHero />
-         <OfferMap />
-         <div className="w-full theme-container !py-10">
-            <div className="flex flex-wrap md:flex-nowrap items-center justify-between gap-4 mb-10">
-               <div className="">
-                  <div className="text-2xl lg:text-3xl font-semibold">Fall into travel</div>
-                  <p className='w-full lg:w-[80%] text-base font-normal'>Going somewhere to celebrate this season? Whether you’re going home or somewhere to roam, we’ve got the travel tools to get you to your destination.</p>
+      <Suspense>
+         <div className='w-full mb-10'>
+            <FlightHero />
+            <OfferMap />
+            <div className="w-full theme-container !py-10">
+               <div className="flex flex-wrap md:flex-nowrap items-center justify-between gap-4 mb-10">
+                  <div className="">
+                     <div className="text-2xl lg:text-3xl font-semibold">Fall into travel</div>
+                     <p className='w-full lg:w-[80%] text-base font-normal'>Going somewhere to celebrate this season? Whether you’re going home or somewhere to roam, we’ve got the travel tools to get you to your destination.</p>
+                  </div>
+                  <Link href='/' className='text-sm text-[#112211] whitespace-nowrap border border-[#8DD3BB] rounded py-2 px-4'>See All</Link>
                </div>
-               <Link href='/' className='text-sm text-[#112211] whitespace-nowrap border border-[#8DD3BB] rounded py-2 px-4'>See All</Link>
-            </div>
-            <div className="flex flex-wrap -m-2">
-               {
-                  flightOffers?.map((offer, index) =>
-                     <div key={index} className="w-full md:w-6/12 lg:w-3/12 p-2">
-                        <div style={{ backgroundImage: `url('${offer?.image}')` }} className="w-full relative aspect-[300/450] bg-cover bg-center rounded-xl">
-                           <div className="absolute left-0 right-0 bottom-0 w-full p-6">
-                              <div className="w-full flex flex-nowrap justify-between gap-4 mb-4">
-                                 <ul>
-                                    <li className="text-2xl font-medium text-white">{offer?.location}</li>
-                                    <li className='text-base text-white font-normal'>{offer?.description}</li>
-                                 </ul>
-                                 <div className="text-2xl font-medium text-white">${offer?.price}</div>
+               <div className="flex flex-wrap -m-2">
+                  {
+                     flightOffers?.map((offer, index) =>
+                        <div key={index} className="w-full md:w-6/12 lg:w-3/12 p-2">
+                           <div style={{ backgroundImage: `url('${offer?.image}')` }} className="w-full relative aspect-[300/450] bg-cover bg-center rounded-xl">
+                              <div className="absolute left-0 right-0 bottom-0 w-full p-6">
+                                 <div className="w-full flex flex-nowrap justify-between gap-4 mb-4">
+                                    <ul>
+                                       <li className="text-2xl font-medium text-white">{offer?.location}</li>
+                                       <li className='text-base text-white font-normal'>{offer?.description}</li>
+                                    </ul>
+                                    <div className="text-2xl font-medium text-white">${offer?.price}</div>
+                                 </div>
+                                 <Link href={offer?.link} className='w-full inline-flex justify-center text-sm text-[#112211] font-medium bg-[#8DD3BB] rounded py-2 px-4'>Book Flight</Link>
                               </div>
-                              <Link href={offer?.link} className='w-full inline-flex justify-center text-sm text-[#112211] font-medium bg-[#8DD3BB] rounded py-2 px-4'>Book Flight</Link>
                            </div>
                         </div>
-                     </div>
-                  )
-               }
+                     )
+                  }
 
-            </div>
-         </div>
-         <div className="w-full theme-container !py-10">
-            <div className="flex flex-wrap md:flex-nowrap items-center justify-between gap-4 mb-10">
-               <div className="">
-                  <div className="text-2xl lg:text-3xl font-semibold">Fall into travel</div>
-                  <p className='w-full lg:w-[80%] text-base font-normal'>Going somewhere to celebrate this season? Whether you’re going home or somewhere to roam, we’ve got the travel tools to get you to your destination.</p>
                </div>
-               <Link href='/' className='text-sm text-[#112211] whitespace-nowrap border border-[#8DD3BB] rounded py-2 px-4'>See All</Link>
             </div>
-            <div className="flex flex-wrap -m-3">
-               <div className="w-full  lg:w-6/12 p-3">
-                  <div className="bg-[#8DD3BB] flex flex-col aspect-[652/424] rounded-2xl p-6">
-                     <div className="flex flex-wrap justify-between gap-4 mb-2">
-                        <div className='w-full md:w-6/12 text-[40px] font-bold text-[#112211]'>Backpacking Sri Lanka</div>
-                        <div className="">
-                           <div className="bg-white text-center rounded-md p-2">
-                              <p className='text-sm font-normal'>From</p>
-                              <p className='text-xl font-semibold font-montserrat'>$700</p>
-                           </div>
-                        </div>
-                     </div>
-                     <div className="min-h-44 flex-grow">
-                        <p className='text-sm font-normal text-[#112211]'>Traveling is a unique experience as it&apos;s the best way to unplug from the pushes and pulls of daily life. It helps us to forget about our problems, frustrations, and fears at home. During our journey, we experience life in different ways. We explore new places, cultures, cuisines, traditions, and ways of living.</p>
-                     </div>
-                     <Link href='/' className='w-full inline-flex justify-center text-sm text-[#112211] font-medium bg-white rounded py-2 px-4'>Book Flight</Link>
+            <div className="w-full theme-container !py-10">
+               <div className="flex flex-wrap md:flex-nowrap items-center justify-between gap-4 mb-10">
+                  <div className="">
+                     <div className="text-2xl lg:text-3xl font-semibold">Fall into travel</div>
+                     <p className='w-full lg:w-[80%] text-base font-normal'>Going somewhere to celebrate this season? Whether you’re going home or somewhere to roam, we’ve got the travel tools to get you to your destination.</p>
                   </div>
+                  <Link href='/' className='text-sm text-[#112211] whitespace-nowrap border border-[#8DD3BB] rounded py-2 px-4'>See All</Link>
                </div>
-               <div className="w-full lg:w-6/12 p-3">
-                  <div className="flex flex-wrap -m-2">
-                     {
-                        flightOffers?.map((offer, index) =>
-                           <div key={index} className="w-full md:w-6/12 p-2">
-                              <div style={{ backgroundImage: `url('${offer?.image}')` }} className="w-full relative aspect-[318/200] bg-cover bg-center border-2 border-[#8DD3BB] rounded-xl">
+               <div className="flex flex-wrap -m-3">
+                  <div className="w-full  lg:w-6/12 p-3">
+                     <div className="bg-[#8DD3BB] flex flex-col aspect-[652/424] rounded-2xl p-6">
+                        <div className="flex flex-wrap justify-between gap-4 mb-2">
+                           <div className='w-full md:w-6/12 text-[40px] font-bold text-[#112211]'>Backpacking Sri Lanka</div>
+                           <div className="">
+                              <div className="bg-white text-center rounded-md p-2">
+                                 <p className='text-sm font-normal'>From</p>
+                                 <p className='text-xl font-semibold font-montserrat'>$700</p>
                               </div>
                            </div>
-                        )
-                     }
+                        </div>
+                        <div className="min-h-44 flex-grow">
+                           <p className='text-sm font-normal text-[#112211]'>Traveling is a unique experience as it&apos;s the best way to unplug from the pushes and pulls of daily life. It helps us to forget about our problems, frustrations, and fears at home. During our journey, we experience life in different ways. We explore new places, cultures, cuisines, traditions, and ways of living.</p>
+                        </div>
+                        <Link href='/' className='w-full inline-flex justify-center text-sm text-[#112211] font-medium bg-white rounded py-2 px-4'>Book Flight</Link>
+                     </div>
+                  </div>
+                  <div className="w-full lg:w-6/12 p-3">
+                     <div className="flex flex-wrap -m-2">
+                        {
+                           flightOffers?.map((offer, index) =>
+                              <div key={index} className="w-full md:w-6/12 p-2">
+                                 <div style={{ backgroundImage: `url('${offer?.image}')` }} className="w-full relative aspect-[318/200] bg-cover bg-center border-2 border-[#8DD3BB] rounded-xl">
+                                 </div>
+                              </div>
+                           )
+                        }
+                     </div>
                   </div>
                </div>
             </div>
          </div>
-      </div>
+      </Suspense>
    )
 }
 
