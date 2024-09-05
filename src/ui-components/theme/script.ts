cfd3a7800ce266c8c42139c232050542dee5ad0f
Arup Maity <arupmaity079@gmail.com>
2024-09-05 13:20:39 +0530

    solve typescript type error

--- src/ui-components/theme/script.ts
+++ src/ui-components/theme/script.ts
@@ -1,50 +1,49 @@
 export const script = (
-   attribute,
-   storageKey,
-   defaultTheme,
-   forcedTheme,
-   themes,
-   value,
-   enableSystem,
-   enableColorScheme
- ) => {
+   attribute: string,
+   storageKey: string,
+   defaultTheme: any,
+   forcedTheme: any,
+   themes: any,
+   value: any,
+   enableSystem: any,
+   enableColorScheme: any
+) => {
    const el = document.body
    const systemThemes = ['light', 'dark']
    const isClass = attribute === 'class'
-   const classes = isClass && value ? themes.map(t => value[t] || t) : themes
- 
+   const classes = isClass && value ? themes.map((t: any) => value[t] || t) : themes
+
    function updateDOM(theme: string) {
-     if (isClass) {
-       el.classList.remove(...classes)
-       el.classList.add(theme)
-     } else {
-       el.setAttribute(attribute, theme)
-     }
- 
-     setColorScheme(theme)
+      if (isClass) {
+         el.classList.remove(...classes)
+         el.classList.add(theme)
+      } else {
+         el.setAttribute(attribute, theme)
+      }
+
+      setColorScheme(theme)
    }
- 
+
    function setColorScheme(theme: string) {
-     if (enableColorScheme && systemThemes.includes(theme)) {
-       el.style.colorScheme = theme
-     }
+      if (enableColorScheme && systemThemes.includes(theme)) {
+         el.style.colorScheme = theme
+      }
    }
- 
+
    function getSystemTheme() {
-     return window.matchMedia('(prefers-color-scheme: dark)').matches ? 'dark' : 'light'
+      return window.matchMedia('(prefers-color-scheme: dark)').matches ? 'dark' : 'light'
    }
- 
+
    if (forcedTheme) {
-     updateDOM(forcedTheme)
+      updateDOM(forcedTheme)
    } else {
-     try {
-       const themeName = localStorage.getItem(storageKey) || defaultTheme
-       const isSystem = enableSystem && themeName === 'system'
-       const theme = isSystem ? getSystemTheme() : themeName
-       updateDOM(theme)
-     } catch (e) {
-       //
-     }
+      try {
+         const themeName = localStorage.getItem(storageKey) || defaultTheme
+         const isSystem = enableSystem && themeName === 'system'
+         const theme = isSystem ? getSystemTheme() : themeName
+         updateDOM(theme)
+      } catch (e) {
+         //
+      }
    }
- }
- 
+}
